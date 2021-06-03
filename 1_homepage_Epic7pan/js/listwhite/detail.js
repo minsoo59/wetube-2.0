94cb141584fbc34896dc3b75221987c857760cc7
minsoo59 <73215507+minsoo59@users.noreply.github.com>
2021-06-03 15:16:17 +0900

    commit

--- 1_homepage_Epic7pan/js/listwhite/detail.js
+++ 1_homepage_Epic7pan/js/listwhite/detail.js
@@ -1,13 +1,8 @@
 const listWhite = document.getElementById("listWhite"),
   menu = listWhite.querySelector("li:nth-child(1)"),
   modal = document.getElementById("modalDetail"),
-<<<<<<< HEAD
   btn = modal.querySelector("button"),
   overlay = modal.querySelector(".overlay"),
-=======
-  btn = modalDetail.querySelector("button"),
-  overlay = modalDetail.querySelector(".overlay"),
->>>>>>> ab560b1cb87f258c5ff105a05e5b63f6b51d3de5
   prev = document.getElementById("prev"),
   next = document.getElementById("next"),
   videoList = document.getElementById("videoList"),
@@ -32,10 +27,7 @@ function init() {
   menu.addEventListener("click", dClick);
   btn.addEventListener("click", dout);
   overlay.addEventListener("click", dout);
-<<<<<<< HEAD
   // slide button
-=======
->>>>>>> ab560b1cb87f258c5ff105a05e5b63f6b51d3de5
   prev.addEventListener("click", prevHandler);
   next.addEventListener("click", nextHandler);
 }
--- 1_homepage_Epic7pan/js/listwhite/startegy.js
+++ 1_homepage_Epic7pan/js/listwhite/startegy.js
@@ -1,5 +1,4 @@
 const listWhite = document.getElementById("listWhite"),
-<<<<<<< HEAD
   menu = listWhite.querySelector("li:nth-child(3)"),
   modal = document.getElementById("modalStrategy"),
   overlay = modal.querySelector(".overlay"),
@@ -13,25 +12,6 @@ function uListSeclected(event) {
   uChild[selecIndex].classList.add("selected");
 }
 let mClass = modal.classList;
-=======
-  strategy = listWhite.querySelector("li:nth-child(3)"),
-  modalStrategy = document.getElementById("modalStrategy"),
-  overlayStrategy = modalStrategy.querySelector(".overlay"),
-  uploadList = document.getElementById("upload_list"),
-  uploadChild = uploadList.querySelectorAll("li");
-
-//strategy
-function uploadListSeclected(event) {
-  // for (let i = 0; i < uploadChild.length; i++) {
-  //   uploadChild[i].classList.remove("selected");
-  // }
-  uploadChild.forEach((list) => list.classList.remove("selected"));
-  let p = event.target.parentElement;
-  let selecIndex = Array.prototype.indexOf.call(p.children, event.target);
-  uploadChild[selecIndex].classList.add("selected");
-}
-let mClass = modalStrategy.classList;
->>>>>>> ab560b1cb87f258c5ff105a05e5b63f6b51d3de5
 let h = "hiddenStrategy";
 function sOut() {
   mClass.add(h);
@@ -41,7 +21,6 @@ function sClick() {
 }
 
 function init() {
-<<<<<<< HEAD
   menu.addEventListener("click", sClick);
   overlay.addEventListener("click", sOut);
 
@@ -49,11 +28,6 @@ function init() {
   // uChild[0].classList.add("selected");
 
   uList.addEventListener("click", uListSeclected);
-=======
-  strategy.addEventListener("click", sClick);
-  overlayStrategy.addEventListener("click", sOut);
-  uploadList.addEventListener("click", uploadListSeclected);
->>>>>>> ab560b1cb87f258c5ff105a05e5b63f6b51d3de5
 }
 
 init();
