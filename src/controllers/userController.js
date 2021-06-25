ae1eded27ef699f8c7742e6a12ab35954d210896
minsoo59 <73215507+minsoo59@users.noreply.github.com>
2021-06-25 17:42:47 +0900

    edit profile first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -150,5 +150,10 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/wetube");
 };
-export const edit = (req, res) => res.send("Edit User!");
+export const getEdit = (req, res) => {
+  return res.render("wetubeEdit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  return res.render("wetubeEdit-profile");
+};
 export const see = (req, res) => res.send("see User!");
--- src/public/2.cloneCoding/02. toDoList/Js/05.weather.js
+++ src/public/2.cloneCoding/02. toDoList/Js/05.weather.js
@@ -3,22 +3,35 @@ const weather = document.querySelector(".js-weather");
 const CORDS = "cords";
 const API_KEY = "485152cb1a7ef3bee03f2324cbf22ae4";
 
-function getWeather(lat, log) {
-  // fetch함수는 외부의 자료를 request 함수
-  // then은 기본적으로 함수호출이지만 데이터가 완전히 들어온 다음 호출하는것.
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
-  )
-    .then(function (response) {
-      // 대기상태(가져온 데이터를 처리중)가 됨. 그럼 than을 써야함
-      // console.log(response.json());
-      return response.json();
+// function getWeather(lat, log) {
+//   // fetch함수는 외부의 자료를 request 함수
+//   // then은 기본적으로 함수호출이지만 데이터가 완전히 들어온 다음 호출하는것.
+//   fetch(
+//     `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`
+//   )
+//     .then(function (response) {
+//       // 대기상태(가져온 데이터를 처리중)가 됨. 그럼 than을 써야함
+//       // console.log(response.json());
+//       return response.json();
+//     })
+//     .then(function (json) {
+//       const temperature = json.main.temp;
+//       const place = json.name;
+//       weather.innerText = `${temperature} @ ${place}`;
+//     });
+// }
+
+async function getWeather(lat, log) {
+  let weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  await (
+    await fetch(weatherUrl, {
+      function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+      },
     })
-    .then(function (json) {
-      const temperature = json.main.temp;
-      const place = json.name;
-      weather.innerText = `${temperature} @ ${place}`;
-    });
+  ).json();
 }
 
 function saveCoords(coordObj) {
@@ -30,8 +43,7 @@ function handleGetSuccess(position) {
   const longitude = position.coords.longitude;
   const coordObj = {
     // 객체에 변수의 이름과 객체의key의 이름을 같게 저장할때는
-    // latitude : latitude,
-    // longitude : longitude
+    // latitude : latitude, longitude : longitude
     latitude,
     longitude,
   };
