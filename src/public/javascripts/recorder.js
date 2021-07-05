7409d72e30eeabc235d80c3527f86dd610edc933
minsoo59 <73215507+minsoo59@users.noreply.github.com>
2021-07-05 16:43:08 +0900

    ﻿Video Preview

--- src/public/javascripts/recorder.js
+++ src/public/javascripts/recorder.js
@@ -1,11 +1,12 @@
 const startBtn = document.getElementById("startBtn");
-
+const video = document.getElementById("preview");
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: true,
+    video: { width: 200, height: 200 },
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
